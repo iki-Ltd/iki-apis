2adf5de0dff11a82927c4c5a25704a7bc8ab9abc
mishobo <husseinabdalla287@gmail.com>
2023-12-09 13:55:50 +0300

    digital ocean depolyment

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:17-alpine
-COPY target/iki.jar iki.jar
+COPY target/iki-backend-0.0.1-SNAPSHOT.jar iki.jar
 ENTRYPOINT ["java", "-jar","/iki.jar"]
--- src/main/java/com/iki/backend/repository/RoleRepository.java
+++ src/main/java/com/iki/backend/repository/RoleRepository.java
@@ -2,12 +2,14 @@ package com.iki.backend.repository;
 
 import com.iki.backend.model.ERole;
 import com.iki.backend.model.Role;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+//@Transactional
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(ERole name);
 }
